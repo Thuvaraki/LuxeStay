339d555f0ffed7d7bdd1632e5fca024aaf4b9fa1
Thuvaraki <108112931+Thuvaraki@users.noreply.github.com>
2024-05-08 21:03:30 +0530

    Create RoomResponseDTO.java

---
+++ backend/src/main/java/com/project/backend/ResponseDTO/RoomResponseDTO.java
@@ -0,0 +1,34 @@
+package com.project.backend.ResponseDTO;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.Base64;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+public class RoomResponseDTO {
+    private Long id;
+    private String roomType;
+    private BigDecimal roomPrice;
+    private String photo; //String representation can be easily managed using standard JSON or other serialization techniques.
+    private boolean isBooked;
+    private List<BookedRoomResponseDTO> bookings;
+
+    public RoomResponseDTO(Long id, String roomType, BigDecimal roomPrice) {
+        this.id = id;
+        this.roomType = roomType;
+        this.roomPrice = roomPrice;
+    }
+
+    public RoomResponseDTO(Long id, String roomType, BigDecimal roomPrice, byte[] photoBytes, boolean isBooked, List<BookedRoomResponseDTO> bookings) {
+        this.id = id;
+        this.roomType = roomType;
+        this.roomPrice = roomPrice;
+        this.photo = photoBytes != null ? Base64.getEncoder().encodeToString(photoBytes) : null;
+        this.isBooked = isBooked;
+        this.bookings = bookings;
+    }
+}
