98acc1750ab5f2cb6e0f8abf2fe8841e38dce464
Thuvaraki <108112931+Thuvaraki@users.noreply.github.com>
2024-05-10 15:08:58 +0530

    Routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,14 @@ import { Routes, Route } from "react-router-dom";
 import AddRoom from "./Components/Room/AddRoom";
 import ExistingRoom from "./Components/Room/ExistingRoom";
 import EditRoom from "./Components/Room/EditRoom";
+import Home from "./Components/Home/Home";
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<ExistingRoom />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/existing-rooms" element={<ExistingRoom />} />
         <Route path="/add-room" element={<AddRoom />} />
         <Route path="/edit-room/:roomId" element={<EditRoom />} />
       </Routes>
---
+++ frontend/src/Components/Room/EditRoom.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const EditRoom = () => {
+  return <div>edit/view</div>;
+};
+
+export default EditRoom;
