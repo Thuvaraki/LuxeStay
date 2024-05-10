8bd7d27772d29a09a9779f6d1e965b7a8dba85d4
Thuvaraki <108112931+Thuvaraki@users.noreply.github.com>
2024-05-10 14:29:40 +0530

    Routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import AddRoom from "./Components/Room/AddRoom";
+import ExistingRoom from "./Components/Room/ExistingRoom";
+import EditRoom from "./Components/Room/EditRoom";
 
 function App() {
   return (
     <div>
-      <AddRoom />
+      <Routes>
+        <Route path="/" element={<ExistingRoom />} />
+        <Route path="/add-room" element={<AddRoom />} />
+        <Route path="/edit-room/:roomId" element={<EditRoom />} />
+      </Routes>
     </div>
   );
 }
