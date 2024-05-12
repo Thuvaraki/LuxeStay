42a677140b9dbc8ac942653600a4c553c07c803c
Thuvaraki <108112931+Thuvaraki@users.noreply.github.com>
2024-05-12 19:09:54 +0530

    Update RoomCard.jsx

--- frontend/src/Components/Room/RoomCard.jsx
+++ frontend/src/Components/Room/RoomCard.jsx
@@ -8,12 +8,14 @@ const RoomCard = ({ room }) => {
       <Card>
         <Card.Body className="d-flex flex-wrap align-items-center">
           <div className="flex-shrrink-0 mr-3 mb-3 mb-md-0">
-            <Card.Img
-              variant="top"
-              src={`data:image/png;base64, ${room.photo}`}
-              alt="Room Photo"
-              style={{ width: "100%", maxWidth: "200px", height: "auto" }}
-            />
+            <Link to={`/book-room/${room.id}`} className="btn btn-hotel btn-sm">
+              <Card.Img
+                variant="top"
+                src={`data:image/png;base64, ${room.photo}`}
+                alt="Room Photo"
+                style={{ width: "100%", maxWidth: "200px", height: "auto" }}
+              />
+            </Link>
           </div>
           <div className="flex-grow-1 ml-3 px-5">
             <Card.Title>
@@ -27,7 +29,7 @@ const RoomCard = ({ room }) => {
             </Card.Text>
           </div>
           <div className="flex-shrink-0 mt-3">
-            <Link to={`/bookings/${room.id}`} className="btn btn-hotel btn-sm">
+            <Link to={`/book-room/${room.id}`} className="btn-hotel btn-sm btn">
               Book Now
             </Link>
           </div>
