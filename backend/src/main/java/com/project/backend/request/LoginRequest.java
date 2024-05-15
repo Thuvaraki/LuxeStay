063f7554900f6b55d10e6c87dcbb7fa1a7656fa3
Thuvaraki <108112931+Thuvaraki@users.noreply.github.com>
2024-05-15 14:53:09 +0530

    Create LoginRequest.java

---
+++ backend/src/main/java/com/project/backend/request/LoginRequest.java
@@ -0,0 +1,15 @@
+package com.project.backend.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String password;
+}
